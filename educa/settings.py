6e3bbf0c77894039edb2936b3021afc539b5a09f
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-31 05:51:50 +0000

    add the courses app

--- educa/settings.py
+++ educa/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # custom apps
+    'courses.apps.CoursesConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
