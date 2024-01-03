a941f86f874465628b3ebd9756eefbadd4e21dbc
attrams <48452451+Attrams@users.noreply.github.com>
2024-01-03 07:40:36 +0000

    add urls.py file

---
+++ courses/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        route='mine/',
+        view=views.ManageCourseListView.as_view(),
+        name='manage_course_list'
+    ),
+    path(
+        route='create/',
+        view=views.CourseCreateView.as_view(),
+        name='course_create'
+    ),
+    path(
+        route='<pk>/edit/',
+        view=views.CourseUpdateView.as_view(),
+        name='course_edit'
+    ),
+    path(
+        route='<pk>/delete/',
+        view=views.CourseDeleteView.as_view(),
+        name='course_delete'
+    )
+]
