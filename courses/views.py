c41673410a9fa5db6a98529ae62d656ac966878f
attrams <48452451+Attrams@users.noreply.github.com>
2024-01-03 07:57:55 +0000

    add ManageCourseListView class

--- courses/views.py
+++ courses/views.py
@@ -7,6 +7,15 @@ from .models import Course
 # Create your views here.
 
 
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
 class OwnerMixin:
     def get_queryset(self):
         qs = super().get_queryset()
@@ -29,7 +38,7 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     template_name = 'courses/manage/course/form.html'
 
 
-class ManageCourseListView(ListView):
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
     permission_required = 'courses.view_course'
 
