a588c76244dc356ae6fb38bc6f08bbfcda025c4b
attrams <48452451+Attrams@users.noreply.github.com>
2024-01-10 23:17:27 +0000

    add StudentCourseListView and StudentCourseDetailView urls

--- students/urls.py
+++ students/urls.py
@@ -12,5 +12,20 @@ urlpatterns = [
         route='enroll-course/',
         view=views.StudentEnrollCourseView.as_view(),
         name='student_enroll_course'
+    ),
+    path(
+        route='courses/',
+        view=views.StudentCourseListView.as_view(),
+        name='student_course_list'
+    ),
+    path(
+        route='course/<pk>/',
+        view=views.StudentCourseDetailView.as_view(),
+        name='student_course_detail'
+    ),
+    path(
+        route='course/<pk>/<module_id>/',
+        view=views.StudentCourseDetailView.as_view(),
+        name='student_course_detail_module'
     )
 ]
