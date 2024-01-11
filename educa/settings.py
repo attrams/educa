cc97a1e11ee5efa571010dfaf170a946ec5d5cc7
attrams <48452451+Attrams@users.noreply.github.com>
2024-01-11 00:48:33 +0000

    add django-embed-video library

--- educa/settings.py
+++ educa/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed apps
+    'embed_video',
 ]
 
 MIDDLEWARE = [
