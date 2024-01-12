b20e4b131776f382e78c18001af5788e4f5fa100
attrams <48452451+Attrams@users.noreply.github.com>
2024-01-12 03:26:34 +0000

    add django debug toolbar configuration

--- educa/settings.py
+++ educa/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
 
     # installed apps
     'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,3 +146,8 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+# debug toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
